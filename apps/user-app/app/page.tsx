b4862a7e76050a396cdf9cf393ae1ebb05ad565e
avesh-devx <avesh.hasanfatta@devxconsultancy.com>
2025-10-07 22:50:00 +0530

    repo init

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,9 @@
+// import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl text-red-500">Helllllllllll yeah</h1>
+    </div>
+  );
+}
